016339f0da263837d8f0728cfc9d8a0a1aefb214
norgaCerp <suarez.luis@docente.ceibal.edu.uy>
2025-10-06 18:19:15 -0300

    reset
    reset

--- components/footer.php
+++
@@ -1,4 +0,0 @@
-<div class="backFooter"></div>
-<div class="footer">
-    <p>Proyecto interdisciplinario 3° año profesorado de informática - Integrantes: Luis M. Suarez Bennett - Wilton Alfredo Mascarañas Pereira</p>
-</div>
