016e22d8d066310f9d3b3e3aac4ff547028d6826
norgaCerp <suarez.luis@docente.ceibal.edu.uy>
2025-10-06 18:41:19 -0300

    asd

--- pages/salonario/agregarReserva.php
+++
@@ -1,6 +0,0 @@
-
-<button onclick="closeModalForm()">cerrar</button>
-<p>Salon: <?= $_GET["salon"]; ?></p><br>
-<p>posicion: <?= $_GET["pos"]; ?></p>
-<p>dia: <?= $_GET["dia"]; ?></p><br>
-<p>semestre: <?= $_GET["semestre"]; ?></p>
